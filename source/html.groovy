a1678b7b7bd123984aa415ac7d159eb6fdcaa00f
ghector6 <ghector6@Hectors-MacBook-Air.local>
2022-08-31 09:11:40 -0500

    adding head of html

--- source/html.groovy
+++ source/html.groovy
@@ -3,5 +3,9 @@ import groovy.xml.MarkupBuilder
 MarkupBuilder builder = new MarkupBuilder()
 
 builder.html{
-	
+	head{
+		title 'Aboout Hector Garcia'
+		description 'This is part of my Diaspora journal '
+		keywords 'Hector, Groovy, Java, Spring'
+	}
 }
