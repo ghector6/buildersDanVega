89870fc43f17cc80c0fc572268c76b6803289547
ghector6 <ghectormus@gmail.com>
2022-09-01 20:23:12 -0500

    Adding quotes to isbn

--- exerciseDanVega/xmlexercise.groovy
+++ exerciseDanVega/xmlexercise.groovy
@@ -3,17 +3,17 @@ import groovy.xml.MarkupBuilder
 MarkupBuilder builder1 = new MarkupBuilder()
 
 builder1.books{
-	book(isbn: 9781935182443){
+	book(isbn: '978-1935182443'){
 		title "Groovy in Action 2nd Edition"
 		author "Dierk Koenig"
 		price "50.58"
 	}
-	book(isbn:978-1935182948){
+	book(isbn:'978-1935182948'){
 		title "Making Java Groovy"
 		author "Ken Kousen"
 		price "33.96"
 	}
-	book(isbn:978-1937785307){
+	book(isbn:'978-1937785307'){
 		title "Programming Groovy 2: Dynamic Productivity for the Java Developer"
 		auhtor "Venkat Subramaniam"
 		price "28.92"
