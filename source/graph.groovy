11585f20b4a2ca97af4c583d9e21fc82a1b71a4a
ghector6 <ghectormus@gmail.com>
2022-09-03 10:54:17 -0500

    adding working graph builder

--- source/graph.groovy
+++ source/graph.groovy
@@ -9,11 +9,30 @@ class Book{
 
 @ToString(includeNames = true)
 class Section {
-	Section title
+	String title
 	List<Chapter> chapters = []
 }
 
 @ToString(includeNames = true)
 class Chapter {
-	String tilte
+	String title
 } 
+
+ObjectGraphBuilder builder = new ObjectGraphBuilder()
+def book = builder.book(
+		title: "Groovy In Action 2nd Edition", 
+		summary:"blablablalblablalbalblablakfljvvjlvlkjsdv"){
+	 section(title:"Section 1"){
+	 	chapter(title:"Chapter 1")
+	 	chapter(title:"Chapter 2")
+	 	chapter(title:"Chapter 3")
+	 }
+	 section(title:"Section 2"){
+	 	chapter(title:"Chapter 4")
+	 	chapter(title:"Chapter 5")
+	 	chapter(title:"Chapter 6")
+	 }
+
+}
+
+println book
